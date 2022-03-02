fc06e23c026d56b57a038b3428f3fb787f900f00
rafahk2o <49276128+rafahk2o@users.noreply.github.com>
2022-03-02 19:25:51 -0300

    :package:calculo de orçamento

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+
+document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+document.querySelector("#js").addEventListener("change", atualizarPreco)
+document.querySelector("#Layout-sim").addEventListener("change", atualizarPreco)
+document.querySelector("#Layout-nao").addEventListener("change", atualizarPreco)
+document.querySelector("#prazo").addEventListener("change", function (){
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
+    atualizarPreco()
+})
+
+function atualizarPreco(){
+    const qtde = document.querySelector("#qtde").value
+    const temJS = document.querySelector("#js").checked
+    const incluiLayout = document.querySelector("#Layout-sim").checked
+    const prazo = document.querySelector("#prazo").value
+
+
+
+
+    let preco = qtde *100;
+    if (temJS) preco *= 1.1
+    if (incluiLayout) preco += 500
+    let taxaUrgencia = 1 - prazo*0.1;
+    preco *= 1 + taxaUrgencia;
+    
+    
+    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`;
+}
+ 
